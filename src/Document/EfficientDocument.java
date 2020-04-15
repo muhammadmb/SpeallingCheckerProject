dc309f49c59b3ea55d3a53a44511a64d0c35b556
muhammadmb <58988660+muhammadmb@users.noreply.github.com>
2020-04-15 23:26:39 +0200

    Add files via upload

--- src/Document/EfficientDocument.java
+++ src/Document/EfficientDocument.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Document;
 
-/**
- *
- * @author Uswer
- */
-public class EfficientDocument {
+import java.util.List;
+
+public class EfficientDocument extends Document{
+    private int numWords;
+    private int numSentances;
+    private int numSyllables;
+
+    public EfficientDocument(String text) {
+        super(text);
+        processText();
+    }
+
+    private boolean isWord(String tok){
+        return !(tok.indexOf("!") >= 0 || tok.indexOf("?") >= 0 || tok.indexOf(".") >= 0) ;
+    }
     
+    @Override
+    public int getNumWords() {
+        return numWords;
+    }
+
+    @Override
+    public int getNumSentences() {
+        return numSentances;
+    }
+
+    @Override
+    public int getNumSyllables() {
+        return numSyllables;
+    }
+
+    private void processText() {
+        List <String> tokens = gettokens("[1?.]+|[a-zA-Z]+");
+        for(int i =0 ; i < tokens.size() ; i ++){
+            if(isWord(tokens.get(i))){
+                numWords++;
+            }if(tokens.get(i).matches("[.?!]") || i == tokens.size()-1)
+                numSentances++;
+        }numSyllables = countOfSyllables(getText());
+    }
 }
